aa217dc4bb5a67bed231a3520eb7e1292819b28c
archita1146 <55725658+archita1146@users.noreply.github.com>
2020-10-26 18:31:11 -0700

    Update Easy Ones.sql

--- Easy Ones.sql
+++ Easy Ones.sql
@@ -484,5 +484,102 @@ Select month(b.accept_date) as month, coalesce(round(count(distinct b.requester_
 count(distinct a.sender_id, a.send_to_id), 2), 0) as accept_rate from friend_request a, request_accepted b  
 where month (a.request_date) = month(b.accept_date) group by month(accept_date) 
 
+-- 16. CONSECUTIVE AVAILABLE SEATS: 
+-- Several friends at a cinema ticket office would like to reserve consecutive available seats. 
+-- Can you help to query all the consecutive available seats order by the seat_id using the following cinema table? 
+
+| seat_id | free | 
+| 1 | 1    | 
+| 2 | 0    | 
+| 3 | 1    | 
+| 4 | 1    | 
+| 5 | 1    | 
+
+-- Your query should return the following result for the sample case above. 
+| seat_id | 
+|3|
+|4| 
+|5|  
+--Note: 
+--The seat_id is an auto increment int, and free is bool ('1' means free, and '0' means occupied.). 
+--Consecutive available seats are more than 2(inclusive) seats consecutively available. 
+
+ANSWER : 
+SELECT distinct a.seat_id  
+from cinema a  
+JOIN cinema b  
+ON ABS(a.seat_id - b.seat_id) = 1  
+and a.free = true and b.free = true  
+order by a.seat_id;  
+
+#USING WINDOWS FUNCTION 
+select seat_id from  
+( 
+  select *, lag(free, 1, false) over(order by seat_id) as prev_free, 
+  lead(free, 1, false) over(order by seat_id) as next_free from cinema 
+) as t 
+where (free = 1 and prev_free = 1) or (free =1 and next_free = 1) 
+order by seat_id; 
+
+ 
+
+-- 17. SALES PERSON: 
+-- Description 
+-- Given three tables: salesperson, company, orders. 
+-- Output all the names in the table salesperson, who didn’t have sales to company 'RED'. 
+
+-- Example 
+-- Input 
+
+Table: salesperson 
+| sales_id | name | salary | commission_rate | hire_date | 
+|   1      | John | 100000 |     6           | 4/1/2006  | 
+|   2      | Amy  | 120000 |     5           | 5/1/2010  | 
+|   3      | Mark | 65000  |     12          | 12/25/2008| 
+|   4      | Pam  | 25000  |     25          | 1/1/2005  | 
+|   5      | Alex | 50000  |     10          | 2/3/2007  | 
+
+-- The table salesperson holds the salesperson information. Every salesperson has a sales_id and a name. 
+
+Table: company 
+
+| com_id  |  name  |    city    | 
+|   1     |  RED   |   Boston   | 
+|   2     | ORANGE |   New York | 
+|   3     | YELLOW |   Boston   | 
+|   4     | GREEN  |   Austin   | 
+ 
+-- The table company holds the company information. Every company has a com_id and a name. 
+
+Table: orders 
+| order_id | order_date | com_id  | sales_id | amount | 
+| 1        |   1/1/2014 |    3    |    4     | 100000 | 
+| 2        |   2/1/2014 |    4    |    5     | 5000   | 
+| 3        |   3/1/2014 |    1    |    1     | 50000  | 
+| 4        |   4/1/2014 |    1    |    4     | 25000  | 
+ 
+-- The table orders holds the sales record information, salesperson and customer company are represented by sales_id and com_id. 
+
+output 
++------+ 
+| name |  
++------+ 
+| Amy  |  
+| Mark |  
+| Alex | 
++------+ 
+ --Explanation 
+ --According to order '3' and '4' in table orders, it is easy to tell only salesperson 'John' and 'Pam' have sales to company 'RED', 
+--so we need to output all the other names in the table salesperson.  
+
+ANSWER : 
+select s.name from salesperson s where s.sales_id NOT IN ( 
+ select a.sales_id from orders a Left Join company b on a.com_id = b.com_id where b.name = 'RED' 
+); 
+
+select s.name 
+from orders o join company c on (o.com_id = c.com_id and c.name = 'RED') 
+right join salesperson s on s.sales_id = o.sales_id 
+where o.sales_id is null;  
 
 
